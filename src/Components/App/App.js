af020e251afcb937e0075d747196d01a2b79f9ec
Albin Winkelmann <albin-w@hotmail.com>
2019-03-15 12:26:56 +0100

    Creates theme

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import Landing from '../Landing';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    green: {
+      main: '#1DB954',
+    },
+    whote: {
+      main: '#FFFFFF',
+    },
+    black: {
+      main: '#191414',
+    },
+  },
+});
 
 const history = createBrowserHistory();
 
 const App = () => (
-  <Router history={history}>
-    
-  
-  </Router>
+  <div>
+    {/* <Router history={history}> */}
+    <MuiThemeProvider theme={theme}>
+      <Landing theme={theme} />
+    </MuiThemeProvider>
+    {/* </Router> */}
+  </div>
 );
 
 export default App;

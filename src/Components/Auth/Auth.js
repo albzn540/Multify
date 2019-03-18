f043fb5b18bc2a2416fce05dc6144a185c445692
Simon Florén <simflo@kth.se>
2019-03-18 16:50:51 +0100

    Small preparation to validate code

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -51,8 +51,10 @@ const Auth = (props) => {
     };
 
     try {
-      const result = await oauth2.authorizationCode.getToken(tokenConfig)
-      const accessToken = oauth2.accessToken.create(result);
+      // Need to pass the auth variable through props (compose?) from Landing
+      const result = await auth.authorizationCode.getToken(tokenConfig)
+      const accessToken = auth.accessToken.create(result);
+      console.log(accessToken);
     } catch (error) {
       console.log('Access Token Error', error.message);
     }

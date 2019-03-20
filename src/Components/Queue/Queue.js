438112af7a8d1b90891b4bf6b6ef3a72a09ea0c0
Simon Florén <simon.floren@gmail.com>
2019-03-20 17:13:37 +0100

    Added bottom menu bar buttons

--- src/Components/Queue/Queue.js
+++ src/Components/Queue/Queue.js
@@ -12,6 +12,8 @@ import {
   ListItemSecondaryAction,
   SvgIcon,
   IconButton,
+  AddIcon,
+  Fab,
 } from '@material-ui/core';
 
 const styles = theme => ({
@@ -28,6 +30,9 @@ const styles = theme => ({
     color: theme.palette.textPrimary.main,
     fontWeight: '500',
     fontSize: '0.9375rem',
+  },
+  fab: {
+    margin: theme.spacing.unit,
   },
 });
 
@@ -60,14 +65,7 @@ const Queue = (props) => {
           </Typography>
         </Grid>
       </Grid>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justify="center"
-        className={classes.root}
-        spacing={16}
-      >
+      <Grid item>
         <List dense={false}>
           {songs.map(song => (
             <ListItem>
@@ -83,18 +81,37 @@ const Queue = (props) => {
               <ListItemSecondaryAction>
                 <IconButton aria-label="Delete">
                   <SvgIcon>
-                    <path fill="#FFFFFF" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
+                    <path
+                      fill="#FFFFFF"
+                      d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z"
+                    />
                   </SvgIcon>
                 </IconButton>
                 <IconButton aria-label="Delete">
                   <SvgIcon>
-                    <path fill="#FFFFFF" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                    <path
+                      fill="#FFFFFF"
+                      d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
+                    />
                   </SvgIcon>
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
           ))}
         </List>
+      </Grid>
+      <Grid item>
+        <IconButton aria-label="Delete">
+          <SvgIcon>
+            <path
+              fill="#000000"
+              d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z"
+            />
+          </SvgIcon>
+        </IconButton>
+        <Fab color="primary" aria-label="Add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
       </Grid>
     </div>
   );

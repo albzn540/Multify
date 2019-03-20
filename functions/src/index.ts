5cfadbd36e22ba6739f718795f2ba8fef82f4f3b
Albin Winkelmann <albin-w@hotmail.com>
2019-03-20 18:30:11 +0100

    Firebase serve now works

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// Admin SDK to access firstore
+admin.initializeApp();
+
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+});
+
+export const authenticateSpotifyUser = functions.https.onCall((data, context) => {
+  console.log("Called auth Spot");
+  console.log(data);
 });

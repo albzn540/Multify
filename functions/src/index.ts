9f80e3628232c851f208df576d92ddf522e3681f
Albin Winkelmann <albin-w@hotmail.com>
2019-03-31 17:57:30 +0200

    Adds random number generator for party codes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
  
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
 export * from './spotify';
+export * from './numberGenerator';
 

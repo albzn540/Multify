56ddc94ce7e94136da579548afbcb053b51a3fe1
Simon Florén <simon.floren@gmail.com>
2019-03-31 16:11:24 +0200

    Use constant instead of custom svg icon

--- src/Components/Search/SearchListItem.js
+++ src/Components/Search/SearchListItem.js
@@ -4,11 +4,11 @@ import {
   ListItem,
   ListItemText,
   ListItemSecondaryAction,
-  SvgIcon,
   IconButton,
   Typography,
 } from '@material-ui/core';
 import { compose } from 'recompose';
+import Add from '../../Constants/Icons/Add';
 
 const styles = theme => ({
   listText: {
@@ -59,12 +59,7 @@ const SearchListItem = (props) => {
             album,
           }, uri)}
         >
-          <SvgIcon>
-            <path
-              fill="#FFFFFF"
-              d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"
-            />
-          </SvgIcon>
+          <Add />
         </IconButton>
       </ListItemSecondaryAction>
     </ListItem>

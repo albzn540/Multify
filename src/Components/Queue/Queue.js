2d8bbad64be1ce9587c95a7934a1e1258fd46c9a
Albin Winkelmann <albin-w@hotmail.com>
2019-04-05 12:19:02 +0200

    Refactors queue

--- src/Components/Queue/Queue.js
+++ src/Components/Queue/Queue.js
@@ -1,40 +1,158 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { compose } from 'recompose';
-import {
-  withStyles,
-  Grid,
-} from '@material-ui/core';
-
-import QueueHeader from './QueueHeader';
-import QueueMenu from './QueueMenu';
-import QueueList from './QueueList';
+import { withStyles, Typography, List } from '@material-ui/core';
+import { withFirebase } from '../../Firebase';
+import { SongListItem, SongItem } from '../SongItem';
 
 const styles = theme => ({
   root: {
-    height: '100vh',
-    backgroundColor: theme.palette.background.main,
+    // display: 'flex',
+    // flexGrow: '1',
+    height: `calc(100% - ${theme.spacing.unit * 8}px)`,
+    padding: theme.spacing.unit,
+  },
+  text: {
+    color: theme.palette.common.white,
   },
 });
 
 const Queue = (props) => {
-  const { classes } = props;
+  const { classes, partyId, firebase } = props;
+
+  const [songs, setSongs] = useState([]);
+
+  const queue = firebase.db
+    .collection('parties')
+    .doc(partyId)
+    .collection('queue');
+
+  useEffect(() => {
+    const unsubscribeParty = firebase.partyQueueRef(partyId).onSnapshot((snap) => {
+      const uid = firebase.currentUser().uid;
+      const newSongs = [];
+      // snap.forEach(songDoc => newSongs.push(songDoc.data()));
+      snap.forEach((songDoc) => {
+        const id = songDoc.data().id;
+        const songObj = songDoc.data();
+        queue.doc(id).collection('likes').doc(uid).get()
+          .then((like) => {
+            if (like.exists) {
+              songObj.liked = true;
+              songObj.disliked = false;
+              newSongs.push(songObj);
+              setSongs(songs.concat(newSongs));
+            } else {
+              queue.doc(id).collection('dislikes').doc(uid).get()
+                .then((dislike) => {
+                  if (dislike.exists) {
+                    songObj.liked = false;
+                    songObj.disliked = true;
+                    newSongs.push(songObj);
+                    setSongs(songs.concat(newSongs));
+                  } else {
+                    songObj.liked = false;
+                    songObj.disliked = false;
+                    newSongs.push(songObj);
+                    setSongs(songs.concat(newSongs));
+                  }
+                })
+                .catch((err) => {
+                  console.error('[Queue] Error checking for dislikes', err);
+                });
+            }
+          })
+          .catch((err) => {
+            console.error('[Queue] Error checking for likes', err);
+          });
+      });
+      // setSongs(songs.concat(newSongs));
+    });
+
+    return () => {
+      unsubscribeParty();
+    };
+  }, []);
+
+  // Make this more DRY?
+  const changeVote = (up, down, id) => {
+    const vote = queue.doc(id);
+    const uid = firebase.currentUser().uid;
+    if (up) {
+      vote.collection('likes').doc(uid).set({})
+        .then(() => {
+          console.log('[Queue] Upvote added');
+        })
+        .catch((err) => {
+          console.error('[Queue] Error adding upvote', err);
+        });
+    } else {
+      vote.collection('likes').doc(uid).delete()
+        .then(() => {
+          console.log('[Queue] Upvote deleted');
+        })
+        .catch((err) => {
+          console.error('[Queue] Error deleting upvote', err);
+        });
+    }
+    if (down) {
+      vote.collection('dislikes').doc(uid).set({})
+        .then(() => {
+          console.log('[Queue] Downvote added');
+        })
+        .catch((err) => {
+          console.error('[Queue] Error adding downvote', err);
+        });
+    } else {
+      vote.collection('dislikes').doc(uid).delete()
+        .then(() => {
+          console.log('[Queue] Downvote deleted');
+        })
+        .catch((err) => {
+          console.error('[Queue] Error deleting downvote', err);
+        });
+    }
+  };
 
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justify="center"
-      className={classes.root}
-      spacing={16}
-    >
-      <QueueHeader />
-      <QueueList />
-      <QueueMenu />
-    </Grid>
+    <div className={classes.root}>
+      <Typography variant="h6" className={classes.text}>
+        Now playing
+      </Typography>
+
+      <List>
+        <SongItem
+          key="Give you up"
+          name="Give you up"
+          artists={['Rick']}
+          album="pW0ned"
+          albumUrl="https://www.femalefirst.co.uk/image-library/square/1000/r/rick-astley-whenever-you-need-somebody-album-cover.jpg"
+        />
+      </List>
+
+      <Typography variant="h6" className={classes.text}>
+        Queue
+      </Typography>
+
+      <List>
+        {songs.map(song => (
+          <SongListItem
+            key={song.name}
+            name={song.name}
+            artists={song.artists}
+            album={song.album.name}
+            albumUrl={song.album.images[2].url}
+            id={song.id}
+            changeVote={changeVote}
+            upvoteBefore={song.liked}
+            downvoteBefore={song.disliked}
+          />
+        ))}
+      </List>
+    </div>
   );
 };
 
 export default compose(
   withStyles(styles),
+  withFirebase,
 )(Queue);

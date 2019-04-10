c554968b759bed768749c7fa34791a6c2c6e10a8
Albin Winkelmann <albin-w@hotmail.com>
2019-04-10 23:09:18 +0200

    Redid Spotify login procedure

--- src/Spotify/Spotify.ts
+++ src/Spotify/Spotify.ts
@@ -1,5 +1,6 @@
 import SpotifyWebApi from 'spotify-web-api-js';
 import oauth2 from 'client-oauth2';
+import uuidv4 from 'uuid/v4';
 
 declare class Firebase {
   db: firebase.firestore.Firestore;
@@ -32,7 +33,8 @@ let tokenExpiresIn: number;
 
 class Spotify {
   client: SpotifyWebApi.SpotifyWebApiJs;
-  spotifyUser: SpotifyApi.CurrentUsersProfileResponse | undefined; 
+  spotifyUser: SpotifyApi.CurrentUsersProfileResponse | undefined;
+  uuid: string;
 
   constructor(firebase: Firebase) {
     // Initialize private stuff
@@ -40,11 +42,15 @@ class Spotify {
     tokenExpiresIn = 0;
 
     // Public stuff
+    this.uuid = uuidv4();
     this.client = new SpotifyWebApi();
 
     // Check for stored data
     const storedData = localStorage.getItem('spotify_data');
+    const uuid = localStorage.getItem('uuid');
+
     if (storedData) {
+      // Stored data exists, user has logged in before
       const spotifyData = JSON.parse(storedData);
       console.log('[Spotify] Stored data', spotifyData);
       const currentTime = Math.round(Date.now() / 1000) + 30; // add 30s buff time
@@ -56,59 +62,84 @@ class Spotify {
         refreshToken = spotifyData.refresh_token;
         setTimeout(this.refreshTokenCallback, tokenExpiresIn * 1000 - 3000);
         this.client.setAccessToken(spotifyData.access_token);
-        this.getSpotifyUser();
       }
+      this.client.getMe().then(user => {
+        this.spotifyUser = user;
+        this.uuid = user.id;
+      }).catch(err => console.log("[Spotify] Couldn't fetch Spotify user"))
+    } else {
+      //Anoymous user, use old uuid if there is one
+      this.uuid = uuid || this.uuid;
+      console.debug('[Spotify] Anonymous user', this.uuid);
     }
   }
 
-  getSpotifyUser = () => {
-    this.client.getMe().then(user => {
+  /**
+   * Requests Spotify user data and saves it to Spotify instance
+   */
+  getNewSpotifyUser = async () => {
+    return this.client.getMe().then(user => {
       this.spotifyUser = user;
-    }).catch((e: any) => {
-      console.error("Couldn't get Spotify user", e);
-    })
-  };
+      this.uuid = user.id;
+    });
+  }
 
-  // Might not be needed anymore
-  // spotifyUser = () => {
-  //   const accToken = this.client.getAccessToken();
-  //   console.log('[Spotify][spotifyUser]', accToken);
-  //   return accToken === '' ? null : accToken;
-  // };
-
+  /** 
+   * Authorize a Spotify user with Spotify popup window (Retrieve access token).
+   * 
+   * The function will check the current url and see if it contains the oauth
+   * code needed for authorization. If the code is missing from the url, we need
+   * to be redirected to the spotify popup window.
+   * After logging in with our Spotify account, we will be redirected back to
+   * the login page. This time with the code in the url needed for the
+   * autorization
+   * 
+   * @param {string} url - Window URL
+  */
   authorizeWithSpotify = async (url: string = '') => {
     console.log('[Spotify][authorizeWithSpotify]');
 
-    if (!this.spotifyUser) {
-      const authenticate = fb.functions.httpsCallable('authenticateSpotifyUser');
-
-      return authenticate({ url }).then(async response => {
-        console.log('[Spotify][authorizeWithSpotify] Authorized! Now authenticating...');
-        const { data: { access_token, refresh_token, expires_in } } = response;
-        tokenExpiresIn = expires_in;
-        refreshToken = refresh_token;
-        this.client.setAccessToken(access_token);
-        this.saveToLocalStorage();
-      }).catch(e => {
-        console.error(e);
-        const uri = authClient.code.getUri();
-        // console.log("Right now, since we have to pay Firebase, we need to restrict the number of requests");
-        // console.log("In the future this way of authentication will be removed");
-        // console.log("Paste this url into the browser: ", uri);
-        // console.info('[Spotify][authorizeWithSpotify] Not yet authorizing. Now autorizing...');
-        window.location.assign(uri);
-      });
-    } else {
+    if(this.spotifyUser) {
       return Promise.resolve('Already authenticated');
     }
+
+    if (!url.includes('?code=')) {
+      // code was missing from url
+      const uri = authClient.code.getUri();
+      window.location.assign(uri);
+      // Uncomment if you're experiencing a refresh loop  
+      // console.log("Right now, since we have to pay Firebase, we need to restrict the number of requests");
+      // console.log("In the future this way of authentication will be removed");
+      // console.log("Paste this url into the browser: ", uri);
+      // console.info('[Spotify][authorizeWithSpotify] Not yet authorizing. Now autorizing...');
+    }
+
+    // Get firebase function
+    const authenticate = fb.functions.httpsCallable('authenticateSpotifyUser');
+    return authenticate({ url }).then(async response => {
+      console.log(url);
+      console.log('[Spotify][authorizeWithSpotify] Authorized! Now authenticating...');
+      const { data: { access_token, refresh_token, expires_in } } = response;
+      tokenExpiresIn = expires_in;
+      refreshToken = refresh_token;
+      this.client.setAccessToken(access_token);
+      this.getNewSpotifyUser();
+      this.saveToLocalStorage();
+    }).catch(err => console.error(err));
   };
 
+  /** 
+   * Main user login function.
+   *  - Authenticates the spotify user if neccessary (spotify login popup) 
+   *  - Retrieves access token
+   *  - Logs user into Firebase
+   * 
+   * @param {string} url - Window URL
+   */
   loginUser = async (url: string = '') => {
-    // Check if we're already logged in
-    // Check if we already have an access token
-    if (!this.spotifyUser) {
-      // Retrieve Spotify code
-      
+
+    if(!this.spotifyUser) {
+      // Log in spotify user to retrieve access token
       console.log('[Spotify][loginUser] Retrieve accesstoken...', this.spotifyUser);
       await this.authorizeWithSpotify(url).catch(err => {
         return Promise.resolve(err);
@@ -120,12 +151,21 @@ class Spotify {
       return Promise.resolve(fb.currentUser());
     }
 
-    const verifiedUser = await this.client.getMe();
-    this.spotifyUser = verifiedUser; // assign the verified user to spotify client
+    // Log into Firebase
+    if(!this.spotifyUser) {
+      // Make sure we got the data needed
+      await this.getNewSpotifyUser().catch(err => {
+        console.error(err);
+        return Promise.reject()
+      });
+    }
+    
+    if(!this.spotifyUser) {
+      // Check again that we have the needed data
+      return Promise.reject("Could not log in");
+    }
 
-    console.log('[Spotify][loginUser] Verified Spotify user', verifiedUser);
-    const { email, id } = verifiedUser;
-
+    const { email, id, display_name } = this.spotifyUser;
     return fb.auth.signInWithEmailAndPassword(email, id).then(userCredentials => {
       // User exists
       fb.auth.updateCurrentUser(userCredentials.user);
@@ -137,7 +177,7 @@ class Spotify {
       return fb.auth.createUserWithEmailAndPassword(email, id).then(userCredentials => {
         // Save displayname and such
         const user = userCredentials.user;
-        const name = verifiedUser.display_name;
+        const name = display_name;
         if (user != null && name != null) {
           user.updateProfile({
             displayName: name
@@ -154,9 +194,11 @@ class Spotify {
         return user;
       });
     });
-
   }
 
+  /**
+   * Save data to local storage
+   */
   saveToLocalStorage = () => {
     const localStorageData = {
       access_token: this.client.getAccessToken(),
@@ -165,8 +207,14 @@ class Spotify {
     };
 
     localStorage.setItem('spotify_data', JSON.stringify(localStorageData));
+    localStorage.setItem('uuid', this.uuid);
   };
 
+  /**
+   * Invokes Firebase function "refreshToken" to, well, refresh the token
+   * Also, it sets a timeout to call the refreshTokenCallback function again
+   * before the new token runs out of time.
+   */
   refreshTokenCallback = () => {
     console.log('[Spotify][refreshTokenCallback]');
     const refreshFunction = fb.functions.httpsCallable('refreshToken');
@@ -181,21 +229,33 @@ class Spotify {
     setTimeout(this.refreshTokenCallback, tokenExpiresIn * 1000 - 3000);
   };
 
-  createParty = () => {
-    console.debug('[Spotify][createParty] Creating party');
+  /**
+   * Create a new party
+   * Invokes Firebase function "createParty"
+   * 
+   * @param {string} partyname
+   */
+  createParty = (partyname: string) => {
+    console.debug(`[Spotify][createParty] Creating party "${partyname}"`);
     const createPartyFunc = fb.functions.httpsCallable('createParty');
 
     if(!this.spotifyUser) {
-      console.error("You don't seem to be logged in", this.spotifyUser);
+      console.error("[Spotify][createParty] Can't create party if you're not logged in");
       return Promise.reject("You don't seem to be logged in");
     }
+
     return createPartyFunc({
-      name: "Zup",
+      name: partyname,
       spotifyToken: this.client.getAccessToken(),
       spotifyId: this.spotifyUser.id
     });
   };
 
+  /**
+   * Retrieve a party id (document firebase id) by party code (5 numbers)
+   * 
+   * @param {string} code - 5 digit party code
+   */
   getPartyId = async (code: string) => {
     console.debug('[Spotify][getParyId] Retrieving party', code);
     return fb.partiesRef().where('code', '==', code).get().then((snap) => {
@@ -207,11 +267,10 @@ class Spotify {
       if(id) {
         return Promise.resolve(id);
       } else { 
-        return Promise.reject("Could not find party");
+        return Promise.reject(`Could not find party with code=${code}`);
       }
     });
   };
-
 }
 
 export default Spotify;

90a25b1a7e2e8c269aca34d025958d0e825d4ac8
Simon Florén <simon.floren@gmail.com>
2019-04-10 21:06:38 +0200

    Page now loads until results are found

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { compose } from 'recompose';
-import { withStyles, Grid } from '@material-ui/core';
+import { withStyles, Grid, CircularProgress } from '@material-ui/core';
 import SearchBar from './SearchBar';
 import SearchList from './SearchList';
 import { withSpotify } from '../../Spotify';
@@ -13,23 +13,29 @@ const styles = theme => ({
   },
 });
 
-const keyPress = (event, onChange) => {
-  if (event.key === 'Enter') {
-    onChange(event.target);
-    event.preventDefault();
-  }
-};
-
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tracks: [],
       partyId: props.partyId,
+      loading: false,
+      searching: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.addTrack = this.addTrack.bind(this);
   }
+
+  keyPress = (event, onChange) => {
+    if (event.key === 'Enter') {
+      onChange(event.target);
+      this.setState({
+        searching: true,
+        loading: true,
+      });
+      event.preventDefault();
+    }
+  };
 
   handleChange(event) {
     const searchStr = event.value;
@@ -50,6 +56,8 @@ class Search extends React.Component {
         });
         this.setState({
           tracks: items,
+          loading: false,
+          searching: false,
         });
       }, (err) => {
         console.error('[SearchList] Search error:', err);
@@ -82,7 +90,7 @@ class Search extends React.Component {
 
   render() {
     const { classes } = this.props;
-    const { tracks } = this.state;
+    const { tracks, loading, searching } = this.state;
 
     return (
       <Grid
@@ -93,8 +101,12 @@ class Search extends React.Component {
         className={classes.root}
         spacing={16}
       >
-        <SearchBar onChange={this.handleChange} keyPress={keyPress} />
-        <SearchList tracks={tracks} addTrack={this.addTrack} />
+        <SearchBar onChange={this.handleChange} keyPress={this.keyPress} />
+        {loading ? (
+          <CircularProgress color="primary" />
+        ) : (
+          <SearchList tracks={tracks} addTrack={this.addTrack} />
+        )}
       </Grid>
     );
   }

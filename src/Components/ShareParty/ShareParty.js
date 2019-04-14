cb4325bceece5298da37b119aff7a1060fe53259
Albin Winkelmann <albin-w@hotmail.com>
2019-04-14 15:20:54 +0200

    Started working on share party screen

--- src/Components/ShareParty/ShareParty.js
+++ src/Components/ShareParty/ShareParty.js
@@ -1,43 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { compose } from 'recompose';
 import { withStyles, Grid, Typography } from '@material-ui/core';
-import { withFirebase } from '../../Firebase';
+import { withSpotify } from '../../Spotify';
 
 const styles = theme => ({
   root: {
     height: '100vh',
     backgroundColor: theme.palette.background.main,
   },
-  logo: {
-    color: theme.palette.primary.main,
-    paddingBottom: '80px',
-    fontSize: '260px',
-  },
-  text: {
-    color: theme.palette.primary.main,
-  },
 });
 
 const ShareParty = (props) => {
   const {
-    spotify, classes, location,
+    classes, spotify,
   } = props;
 
-  useEffect(() => {
-    const url = location.pathname + location.search;
-    spotify.loginUser(url).then(() => {
-      console.debug('[Login] Creating party...');
-      spotify.createParty(`${spotify.spotifyUser.display_name}'s party`).then((partyData) => {
-        const { data: { code } } = partyData;
-        console.debug('[Login] Party created', partyData);
-        spotify.getPartyId(code).then((partyId) => {
-          window.location.assign(`/party/${partyId}`);
-        });
-      });
-    }).catch((e) => {
-      console.log('[Login]', e);
-    });
-  }, []);
+  const code = 'lel';
 
   return (
     <Grid
@@ -47,12 +25,21 @@ const ShareParty = (props) => {
       justify="center"
       alignItems="center"
     >
-      <Typography>Paty Code:</Typography>
+      <Typography
+        variant="h5"
+      >
+        Paty Code:
+      </Typography>
+      <Typography
+        variant="h5"
+      >
+        {code}
+      </Typography>
     </Grid>
   );
 };
 
 export default compose(
   withStyles(styles),
-  withFirebase,
+  withSpotify,
 )(ShareParty);

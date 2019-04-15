7cf2c2d45e1f42bd1011245cead0286ad777e372
Simon Florén <simon.floren@gmail.com>
2019-04-15 21:27:53 +0200

    Delete room creation page history

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { compose } from 'recompose';
 import { withStyles, Grid, CircularProgress } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
 import { withSpotify } from '../../Spotify';
 import SpotifyLogo from '../../Constants/SpotifyLogo';
 
@@ -21,7 +22,7 @@ const styles = theme => ({
 
 const Login = (props) => {
   const {
-    spotify, classes, location,
+    spotify, classes, location, history,
   } = props;
 
   useEffect(() => {
@@ -32,7 +33,7 @@ const Login = (props) => {
         const { data: { code } } = partyData;
         console.debug('[Login] Party created', partyData);
         spotify.getPartyId(code).then((partyId) => {
-          window.location.assign(`/party/${partyId}`);
+          history.replace({ pathname: `/party/${partyId}` });
         });
       });
     }).catch((e) => {
@@ -61,4 +62,5 @@ const Login = (props) => {
 export default compose(
   withStyles(styles),
   withSpotify,
+  withRouter,
 )(Login);

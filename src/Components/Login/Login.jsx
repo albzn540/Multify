7ddc24aad40251a913b42d7ca6efe0a2ef9039a0
Simon Florén <simon.floren@gmail.com>
2019-04-17 14:44:06 +0200

    Import order

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { compose } from 'recompose';
 import { withStyles, Grid, CircularProgress } from '@material-ui/core';
-import { withSpotify } from '../../Spotify';
 import { withRouter } from 'react-router-dom';
+import { withSpotify } from '../../Spotify';
 import SpotifyLogo from '../../Constants/SpotifyLogo';
 
 const styles = theme => ({

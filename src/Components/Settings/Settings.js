9d954ad60647c8b726f3c11952e42e9ac92ccf48
Albin Winkelmann <albin-w@hotmail.com>
2019-04-17 23:35:53 +0200

    Admin settings are now under settings page

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { compose } from 'recompose';
-import { Typography, withStyles } from '@material-ui/core';
+import { Typography, withStyles, Grid } from '@material-ui/core';
+import SpotifyButton from '../SpotifyButton';
+import { withSpotify } from '../../Spotify';
 
 const styles = theme => ({
+  header: {
+    marginBottom: theme.spacing.unit * 2,
+  },
 });
 
 const Settings = (props) => {
+  const { classes, spotify } = props;
+
+  const startParty = () => {
+    spotify.startParty();
+  };
   return (
     <div>
-      <Typography variant="h4">Settings</Typography>
+      <Typography variant="h4" className={classes.header}>Settings</Typography>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <SpotifyButton value="Start party" onClick={startParty} />
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
 export default compose(
   withStyles(styles),
+  withSpotify,
 )(Settings);

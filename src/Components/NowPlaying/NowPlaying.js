b435624214b8a8c7e38bd0696c4d8d62e6f4cb6e
Albin Winkelmann <albin-w@hotmail.com>
2019-04-18 17:13:31 +0200

    Fixes width issuses on desktop and mobile device for nowplaying

--- src/Components/NowPlaying/NowPlaying.js
+++ src/Components/NowPlaying/NowPlaying.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Typography, withStyles } from '@material-ui/core';
 import { compose } from 'recompose';
+import { isMobile } from 'react-device-detect';
 import NowPlayingSmall from './NowPlayingSmall';
 
 const styles = () => ({
-  root: {
+  desktopRoot: {
     width: '100%',
+  },
+  mobileRoot: {
+    width: '90vw',
   },
 });
 
 const NowPlaying = (props) => {
   const { classes } = props;
   return (
-    <div className={classes.root}>
+    <div className={isMobile ? classes.mobileRoot : classes.desktopRoot}>
       <Typography variant="h6">
         Now playing
       </Typography>
